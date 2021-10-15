14f8218d2f6faef69dfc60fed47572041e6da678
Ben <bmurray@softwareinstitute.com>
2021-10-15 10:51:26 +0100

    More FantasyBook tests

--- src/test/FantasyBookTest.java
+++ src/test/FantasyBookTest.java
@@ -7,11 +7,26 @@ import static org.junit.Assert.assertEquals;
 
 public class FantasyBookTest {
 
+    FantasyBook myFantasyBook = new FantasyBook("Twp Towers", "Tolkirn", "Paperbsck", 3522);
+
     @Test
     public void  testFantasyBookItemTitleMethod() {
-        FantasyBook myFantasyBook = new FantasyBook("Two Towers", "Tolkien", "Paperback", 352);
-        assertEquals("This fantasy is not Two Towers", "Two Towers", myFantasyBook.getItemTitle());
+        assertEquals("This fantasy is not Two Towers.", "Two Towers", myFantasyBook.getItemTitle());
     }
 
+    @Test
+    public void  testFantasyBookWriterNameMethod() {
+        assertEquals("This fantasy is not by Tolkien.", "Tolkien", myFantasyBook.getWriterName());
+    }
+
+    @Test
+    public void  testFantasyBookCoverTypeMethod() {
+        assertEquals("This fantasy is not a paperback.", "Paperback", myFantasyBook.getCoverType());
+    }
+
+    @Test
+    public void  testFantasyBookPagesMethod() {
+        assertEquals("This fantasy is not 352 pages.", 352, myFantasyBook.getPages());
+    }
 
 }
