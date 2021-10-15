8a222ea8a2063478dcb3859fd1a5556d7b1ce89f
Ben <bmurray@softwareinstitute.com>
2021-10-15 13:48:34 +0100

    Trying again to do collection in Library

--- src/com/softwareinstitute/training/murray/ben/Library.java
+++ src/com/softwareinstitute/training/murray/ben/Library.java
@@ -1,4 +1,24 @@
 package com.softwareinstitute.training.murray.ben;
 
+import com.softwareinstitute.training.murray.ben.LibraryItem;
+import java.util.ArrayList;
+
 public class Library {
+
+    /////////////////////////////////////////Attributes////////////////////////////////////////////
+
+    private ArrayList<LibraryItem> items;
+
+    /////////////////////////////////////////Constructors////////////////////////////////////////////
+
+    public Library(ArrayList<LibraryItem> items) {
+        this.items = new ArrayList<>(items);
+    }
+
+    /////////////////////////////////////////Methods////////////////////////////////////////////
+
+    public ArrayList<LibraryItem> getItems() {
+        return items;
+    }
+
 }
--- src/com/softwareinstitute/training/murray/ben/Main.java
+++ src/com/softwareinstitute/training/murray/ben/Main.java
@@ -6,31 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<LibraryItem> items = new ArrayList<>();
+
         System.out.println();
 
-        System.out.println("Book-Fantasy");
-        FantasyBook defaultFantasyBook = new FantasyBook();
-        System.out.println(defaultFantasyBook);
+        LibraryItem defaultFantasyBook = new FantasyBook();
+        LibraryItem defaultBiographyBook = new BiographyBook();
+        LibraryItem defaultDocumentaryDVD = new DocumentaryDVD();
+        LibraryItem defaultAnimationDVD = new AnimationDVD();
+        LibraryItem myFantasyBook = new FantasyBook("Two Towers", "Tolkien", "Paperback", 352);
+
+
+        items.add(defaultFantasyBook);
+        items.add(defaultBiographyBook);
+        items.add(defaultDocumentaryDVD);
+        items.add(defaultAnimationDVD);
+        items.add(myFantasyBook);
+
+        for (LibraryItem i:items) {
+            System.out.println("\n" + i);
+        }
+
         System.out.println();
-
-        System.out.println("Book-Biography");
-        BiographyBook defaultBiographyBook = new BiographyBook();
-        System.out.println(defaultBiographyBook);
         System.out.println();
+        Library allLibrary = new Library(items);
 
-        System.out.println("DVD-Documentary");
-        DocumentaryDVD defaultDocumentaryDVD = new DocumentaryDVD();
-        System.out.println(defaultDocumentaryDVD);
-        System.out.println();
+        System.out.println(allLibrary);
 
-        System.out.println("DVD-Animation");
-        AnimationDVD defaultAnimationDVD = new AnimationDVD();
-        System.out.println(defaultAnimationDVD);
-        System.out.println();
-
-        FantasyBook myFantasy = new FantasyBook("Two Towers", "Tolkien", "Paperback", 352);
-
-        System.out.println(myFantasy);
 
     }
+
+
+
 }
